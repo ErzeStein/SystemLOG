3d6a84e7f7d2fc566e9682e5a1212e869a88f658
ErzeStein <enriques20009@yahoo.com>
2026-06-05 16:11:29 +0200

    Clean up command output in SteinCommand
    Removed Discord support link from command output.

--- src/main/java/de/systemlog/SteinCommand.java
+++ src/main/java/de/systemlog/SteinCommand.java
@@ -13,8 +13,6 @@ public class SteinCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
         sender.sendMessage("§a§lDeveloped by Stein & Mosuree & Namensauswahl");
         sender.sendMessage("§a§lhttps://www.spigotmc.org/resources/systemlog.113233/");
-        sender.sendMessage("§a§lDiscord Support Link");
-        sender.sendMessage("§e§lhttps://discord.gg/B7GwzGGFaP");
         return true;
     }
 }
